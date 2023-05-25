6ef5cd58e8c6fddb54200de07d67c1e16708f048
Hasyim-Kai <ichihatsu1@gmail.com>
2023-05-25 11:09:48 +0700

    add auth & jwt

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,42 +1,37 @@
+import { User } from '../model/user';
 const { JWT_SECRET } = process.env;
-const { User } = require('../models')
 import { compareSync, hashSync } from 'bcrypt';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { Secret, sign } from 'jsonwebtoken';
 
-const login = async (req: Request, res: Response, next: NextFunction) => {
+const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-    return res.status(200).json({ status: true, message: "Login Success" });
-    // const foundExistingEmail = await User.findOne({ where: { email } })
-    // const isPasswordValid = compareSync(password, foundExistingEmail.password);
-    // if (foundExistingEmail && isPasswordValid) {
-    //   const jwtPayload = { id: foundExistingEmail.id, name: foundExistingEmail.name, email, role: foundExistingEmail.role }
-    //   const token = sign(jwtPayload, JWT_SECRET as Secret, { expiresIn: "1d" })
-    //   return res.status(200).json({ status: true, message: "Login Success", data: foundExistingEmail, token });
-    // }
-    // return res.status(200).json({ status: false, message: "Wrong Email/Password", });
+    const foundExistingEmail = await User.findOne({ email }).exec()
+    const isPasswordValid = foundExistingEmail ? compareSync(password, foundExistingEmail?.password) : false;
+    if (foundExistingEmail && isPasswordValid) {
+      const jwtPayload = { id: foundExistingEmail._id, name: foundExistingEmail.name, email }
+      const token = sign(jwtPayload, JWT_SECRET as Secret, { expiresIn: "7d" })
+      const data = { email: foundExistingEmail.email, name: foundExistingEmail.name }
+      return res.status(200).json({ status: true, message: "Login Success", token, data });
+    }
+    return res.status(200).json({ status: false, message: "Wrong Email/Password", });
   } catch (err) {
     console.log(err)
     res.status(500).json({ success: false, error: "Something went wrong", });
   }
 };
 
-const signUp = async (req: Request, res: Response, next: NextFunction) => {
+const signUp = async (req: Request, res: Response) => {
   try {
+    const foundExistingEmail = await User.findOne({ email: req.body.email }).exec();
+    if (foundExistingEmail) {
+      return res.status(200).json({ status: false, message: "Email already exists", });
+    }
     const { name, email, password } = req.body;
-    return res.status(200).json({ status: true, message: "Login Success", name, email, password });
-    //   const foundExistingEmail = await User.findOne({ where: { email } })
-    // if (foundExistingEmail) {
-    //   return res.status(200).json({ status: false, message: "Email already exists", });
-    // } else {
-    //   const newUser = await User.create({ name, email, password: hashSync(password, 10) });
-    //   return res.status(200).json({
-    //     status: true,
-    //     message: "Sign Up Success",
-    //     data: newUser.id
-    //   });
-    // }
+    const newUser = new User({ name, email, password: hashSync(password, 10) });
+    const data = await newUser.save();
+    return res.status(200).json({ status: true, message: "Sign Up Success", id: data._id });
   } catch (err) {
     console.log(err)
     res.status(500).json({ success: false, error: "Something went wrong", });
