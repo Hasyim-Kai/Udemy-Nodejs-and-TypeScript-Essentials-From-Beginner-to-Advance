0331bad4a9bfc2cff42c80158ae3a107ba0bff18
Hasyim-Kai <ichihatsu1@gmail.com>
2023-05-30 16:08:50 +0700

    Add pagination at get movie

--- controllers/movie.controller.ts
+++ controllers/movie.controller.ts
@@ -3,11 +3,22 @@ import { Movie } from '../model/movie';
 
 const getAll = async (req: Request, res: Response) => {
   try {
-    const data = await Movie.find().exec();
+    const page = Number(req.query.page)
+    const itemPerPage = Number(req.query.itemPerPage)
+    if (!page || !itemPerPage) {
+      return res.status(200).json({ status: false, message: 'Param must be exist' });
+    }
+
+    if (isNaN(page) || isNaN(itemPerPage)) {
+      return res.status(200).json({ status: false, message: 'Param must be number' });
+    }
+
+    const moviesToSkip = (page - 1) * itemPerPage
+    const data = await Movie.find().skip(moviesToSkip).limit(itemPerPage).exec();
     return res.status(200).json({ status: true, data });
   } catch (err) {
     console.log(err)
-    res.status(500).json({ success: false, error: "Something went wrong", });
+    return res.status(500).json({ success: false, error: "Something went wrong", });
   }
 };
 
@@ -36,9 +47,11 @@ const add = async (req: Request, res: Response) => {
 const update = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const data = await Movie.updateOne({ id }, req.body).exec();
-    console.log(data.acknowledged)
-    return res.status(201).json({ status: true, data });
+    const data = await Movie.updateOne({ _id: id }, req.body).exec();
+    if (data.acknowledged) {
+      return res.status(201).json({ status: true, message: `Edit Success` });
+    }
+    throw Error()
   } catch (err) {
     console.log(err)
     res.status(500).json({ success: false, error: "Something went wrong", });
