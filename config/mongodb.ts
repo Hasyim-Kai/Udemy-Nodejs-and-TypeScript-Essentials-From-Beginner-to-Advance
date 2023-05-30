7e70bd7544a5fb88991f0dbefa3b03a357fc3e5b
Hasyim-Kai <ichihatsu1@gmail.com>
2023-05-30 11:24:35 +0700

    refactor Mongodb config

--- config/mongodb.ts
+++ config/mongodb.ts
@@ -3,7 +3,6 @@ import { connect, connection } from "mongoose";
 export function connectDB() {
   const options = {
     maxPoolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   };
   connect('mongodb://localhost:27017/udemy_express_practice', options)
