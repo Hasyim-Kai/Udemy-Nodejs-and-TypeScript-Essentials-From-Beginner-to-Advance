185b658b6437796b668e1b0c95d31621fe123929
Hasyim-Kai <ichihatsu1@gmail.com>
2023-05-31 10:04:22 +0700

    refactor comment routes

--- routes/comment.routes.ts
+++ routes/comment.routes.ts
@@ -2,9 +2,8 @@ const router = require("express").Router();
 import comment from "../controllers/comment.controller";
 import { authenticate } from "../middlewares/auth";
 
-router.get("/", authenticate, comment.getAll)
-  .post("/", authenticate, comment.post)
-  .get("/:id", authenticate, comment.get)
+router.post("/", authenticate, comment.post)
+  .get("/:id", authenticate, comment.getAllMovieComment)
   .put("/:id", authenticate, comment.update)
   .delete("/:id", authenticate, comment.del)
 
